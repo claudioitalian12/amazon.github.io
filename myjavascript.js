b95f6f9214b5a147404e5abd6faf50754a99d697
claudioitalian12 <36310714+claudioitalian12@users.noreply.github.com>
2018-05-15 07:35:16 +0100

    initial commit

---
+++ myjavascript.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+        $('.dropdown-toggle').dropdown();
+    });
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function change(e){
+ document.getElementById("dropdownMenu1").innerHTML = e.text;
+}
+
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+
+
+
